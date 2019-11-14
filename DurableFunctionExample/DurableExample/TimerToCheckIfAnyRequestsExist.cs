ca28637c3d0516e751f664103550d289cffbd396
JamieR <github@jamie.pub>
2019-11-14 11:23:59 -0500

    updating code

--- DurableFunctionExample/DurableExample/TimerToCheckIfAnyRequestsExist.cs
+++ DurableFunctionExample/DurableExample/TimerToCheckIfAnyRequestsExist.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -8,9 +9,15 @@ namespace DurableExample
     public static class TimerToCheckIfAnyRequestsExist
     {
         [FunctionName("TimerToCheckIfAnyRequestsExist")]
-        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")]TimerInfo myTimer, [OrchestrationClient]DurableOrchestrationClient starter, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            // Function input comes from the request content.
+            Task instanceId = await starter.StartNewAsync("DurableProcessPriceChange", null);
+            log.Info($"Started orchestration with ID = '{instanceId}'.");
+            return starter.CreateCheckStatusResponse(req, instanceId);
+
         }
     }
 }
